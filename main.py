17e0b0f146636e7e9d937cfa9e85c94cb1320963
Keith Low <keithlowc@gmail.com>
2018-11-02 21:25:15 -0400

    added gas functionality with sensor CCS811

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from config import TwilioKeys
 from src.communications.twilio_coms import Twilio
 from src.temperature.temperature import TemperatureSensor
+from src.gas.gas_sensor import GasSensor
 
 
-def communication(celcius,farenheit):
+def communication(celcius,farenheit,co2,tvoc):
+	data = 'The current Temperature is: {} C, {} F\nThe CO2 levels are: {}\nThe TVOC levels are: {}'.format(celcius,farenheit,co2,tvoc)
+
 	message = Twilio(account_sid= 	TwilioKeys.account_sid,
 					auth_token=		TwilioKeys.auth_token,
 					myPhone=		TwilioKeys.myPhone,
 					TwilioNumber=	TwilioKeys.TwilioNumber,
-					message=		TwilioKeys.message + 'The current temp is: C: ' + str(celcius) + ' F: ' + str(farenheit))
+					message=		TwilioKeys.message + data)
 
 	message.make_call()
 	message.send_text()
@@ -18,8 +21,14 @@ def temp():
 	celcius, farenheit = temp.get_temp()
 	return celcius, farenheit
 
+def gas_sensing():
+	gas = GasSensor()
+	co2, tvoc = gas.get_gas()
+	return co2, tvoc
+
 def main():
 	celcius, farenheit = temp()
-	communication(celcius,farenheit)
+	co2, tvoc = gas_sensing()
+	communication(celcius,farenheit,co2,tvoc)
 
 main()
