bb2713822dd407d9fdfe5a6ee9b3bf34e118c8c2
Keith Low <keithlowc@gmail.com>
2018-12-05 09:40:42 -0500

    added pulse sensing

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from config import TwilioKeys
 from src.communications.twilio_coms import Twilio
 from src.temperature.temperature import TemperatureSensor
 from src.gas.gas_sensor import GasSensor
+from src.pulse_sensing.pulsesensor import Pulsesensor
 
-
-def communication(celcius,farenheit,co2,tvoc):
-	data = 'The current Temperature is: {} C, {} F. The CO2 levels are: {} The TVOC levels are: {}'.format(celcius,farenheit,co2,tvoc)
+def communication(celcius,farenheit,co2,tvoc,pulse):
+	data = 'The current Temperature is: {} C, {} F. The CO2 levels are: {} The TVOC levels are: {} The pulse range is: {}'.format(celcius,farenheit,co2,tvoc,pulse)
 
 	message = Twilio(account_sid= 	TwilioKeys.account_sid,
 					auth_token=		TwilioKeys.auth_token,
@@ -26,9 +26,28 @@ def gas_sensing():
 	co2, tvoc = gas.get_gas()
 	return co2, tvoc
 
+def pulse_sensing():
+	p = Pulsesensor()
+	p.startAsyncBPM()
+	time = 0
+	pulse = []
+
+	while time <= 5:
+		time += 1
+		bpm = p.bpm
+		if bpm > 0:
+			print('BPM: %d' % bpm)
+			pulse.append(bpm)
+		else:
+			print('HEARTBEAT NOT FOUND!')
+		time.sleep(1)
+	p.stopAsyncBPM()
+	return pulse
+
 def main():
 	celcius, farenheit = temp()
 	co2, tvoc = gas_sensing()
-	communication(celcius,farenheit,co2,tvoc)
+	pulse = pulse_sensing()
+	communication(celcius,farenheit,co2,tvoc, pulse)
 
 main()
